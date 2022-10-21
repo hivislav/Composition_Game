0b4285568416cfde8e68bf4117973deb9a28c474
hivislav <hivislav@gmail.com>
2022-10-21 19:04:13 +0600

    task 3 - presentation
    // add resources, layouts and fragments:
    
    - GameFragment
    - GameFinishedFragment
    - ChooseLevelFragment
    - WelcomeFragment

---
+++ app/src/main/java/ru/hivislav/compositiongame/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,39 @@
+package ru.hivislav.compositiongame.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.hivislav.compositiongame.R
+import ru.hivislav.compositiongame.databinding.FragmentChooseLevelBinding
+
+class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding
+        get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance() = ChooseLevelFragment()
+    }
+}
